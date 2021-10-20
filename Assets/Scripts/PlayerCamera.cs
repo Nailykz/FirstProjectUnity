4f1bdadcf2dfed8ddb0151a10907701b672fa7b9
Kilian <74670112+Nailykz@users.noreply.github.com>
2021-10-20 09:48:59 +0200

    FirstCommit

---
+++ Assets/Scripts/PlayerCamera.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCamera : MonoBehaviour
+{
+    private Transform playerTransform {get; set; } = null;
+
+    public Transform _eyeTransform = null;
+
+    private Transform eyeTransform { get { return _eyeTransform; } set { _eyeTransform = value;}}
+    // Start is called before the first frame update
+    public bool _invertCamera = false;
+
+    private bool invertCamera { get { return _invertCamera; } set { _invertCamera = value; }}
+
+    public int valeur { get; private set;}
+    
+    private void Start()
+    {
+        playerTransform = transform;
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        playerTransform.Rotate(Vector3.up,Input.GetAxis("Mouse X"));
+        float invertValue = invertCamera == true ? 1 : -1;
+        eyeTransform.Rotate(Vector3.right,Input.GetAxis("Mouse Y") * -1);
+    }
+}
